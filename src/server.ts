08d30df54fb8382f356f198bfa62261402cf1fac
Felipe Marques <felipe.tecnosuporte@gmail.com>
2025-04-29 01:40:29 -0300

    update "main"

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import cors from "@fastify/cors";
 import { routes } from "./routes";
 
 const app = Fastify({ logger: true });
+
 app.setErrorHandler((error, request, reply) => {
     reply.status(400).send({ error: error.message })
 });
 
 const start = async () => {
-    await app.register(cors) 
+    await app.register(cors, {
+        origin: '*', // or true to allow all
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      }); 
     await app.register(routes)
     try{
         await app.listen({ port: 3333 })
